b08dd8337b009695cc9c71e14eca87d92bcabef0
Pawel Gorczyca <p.gorczyca@cyfronet.pl>
2020-04-02 16:15:20 +0200

    Add matomo tracker for marketplace

--- app/helpers/order_helper.rb
+++ app/helpers/order_helper.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-module OrderNavHelper
+module OrderHelper
   def order_nav_link(title, path, state)
     content_tag(:li, class: "nav-item") do
       link_to_if(state != :disabled, title, path,
@@ -12,6 +12,16 @@ module OrderNavHelper
           content_tag(:div, title, class: "nav-link disabled")
         end
       end
+    end
+  end
+
+  def order_submit_options(step_key, orderable)
+    if orderable && step_key == "summary"
+      { form: "order-form", type: "submit", onclick: "_paq.push(['trackEvent', 'Service', 'Order']);" }
+    elsif step_key == "summary"
+      { form: "order-form", type: "submit", onclick: "_paq.push(['trackEvent', 'Service', 'AddToProject']);" }
+    else
+      { form: "order-form", type: "submit" }
     end
   end
 end
